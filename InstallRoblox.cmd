60ac5e2074e1c4727ad0fc93c80839bb2a82a234
TouseefX <131637263+TouseefX@users.noreply.github.com>
2023-07-14 10:29:16 -0700

    Update InstallRoblox.cmd
    it installs roblox

--- InstallRoblox.cmd
+++ InstallRoblox.cmd
@@ -1,9 +1,12 @@
-@ECHO OFF
 ECHO OFF
 echo if u run it admin it work without no work
-pause
 cd
+echo.
+echo Installing Wget to
 cd C:\Windows\System32
+echo Press any key to start installing
+pause>nul
+rem nul
 curl -LO http://eternallybored.org/misc/wget/1.21.4/64/wget.exe -O wget.exe
 cd
 cd C:\
@@ -12,5 +15,3 @@ cd Roblox
 wget https://roblox.com/download/client
 Ren client Client.exe
 start Client.exe
-
-now save it as cmd
