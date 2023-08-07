06f31a7fb4f461205f233f4888a9f08336526157
TouseefX <131637263+TouseefX@users.noreply.github.com>
2023-08-07 11:01:23 +0400

    Update InstallRoblox.cmd

--- InstallRoblox.cmd
+++ InstallRoblox.cmd
@@ -6,12 +6,11 @@ echo Installing Wget to
 cd C:\Windows\System32
 echo Press any key to start installing
 pause>nul
-rem nul
+del nul
 curl -LO http://eternallybored.org/misc/wget/1.21.4/64/wget.exe -O wget.exe
 cd
 cd C:\
 mkdir Roblox
 cd Roblox
 wget https://roblox.com/download/client
-Ren client Client.exe
-start Client.exe
+Ren C:\Roblox\client Client.exe
