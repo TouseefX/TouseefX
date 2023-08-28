5f211d10502e18144d4c5c9fbb9c09b67bdaa10b
TouseefX <131637263+TouseefX@users.noreply.github.com>
2023-08-29 01:27:19 +0400

    Update InstallBloxstrap.cmd
    added new bloxstrap verison

--- InstallBloxstrap.cmd
+++ InstallBloxstrap.cmd
@@ -1,21 +1,22 @@
 ECHO OFF
 echo if u run it admin it work without no work
-cd
+cd C:\Windows\System32
 echo.
 echo Installing Wget to
-cd C:\Windows\System32
+cd
 echo Press any key to start installing
 pause>nul
 del nul
 curl -LO http://eternallybored.org/misc/wget/1.21.4/64/wget.exe -O wget.exe
 cd C:\
 mkdir Bloxstrap
+cd Bloxstrap
 wget https://download.visualstudio.microsoft.com/download/pr/1146f414-17c7-4184-8b10-1addfa5315e4/39db5573efb029130add485566320d74/windowsdesktop-runtime-6.0.20-win-x64.exe
-run windowsdesktop-runtime-6.0.20-win-x64.exe
+start windowsdesktop-runtime-6.0.20-win-x64.exe
 pause
 echo Start Installing Bloxstrap
 wget https://github.com/pizzaboxer/bloxstrap/releases/download/v2.5.2/Bloxstrap-v2.5.2.exe
-run Bloxstrap-v2.5.2.exe
+start Bloxstrap-v2.5.2.exe
 cd C:\
 mkdir BloxstrapTemp
 cd BloxstrapTemp
@@ -31,4 +32,3 @@ rm windowsdesktop-runtime-6.0.20-win-x64.exe
 rm Bloxstrap-v2.5.2.exe
 cd C:\
 rmdir Bloxstrap
-
