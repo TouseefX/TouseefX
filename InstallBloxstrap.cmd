2833b1d31b65492d81bb1a1839786be7ca17ec90
TouseefX <131637263+TouseefX@users.noreply.github.com>
2023-08-29 01:35:12 +0400

    Update InstallBloxstrap.cmd
    Fix Bug Added Support To Altos Os And Ghost Specte Os And Windows 11 OS fixed app not starting fixed rm command was deleted by Microsoft

--- InstallBloxstrap.cmd
+++ InstallBloxstrap.cmd
@@ -2,6 +2,7 @@ ECHO OFF
 echo if u run it admin it work without no work
 cd C:\Windows\System32
 echo.
+echo.
 echo Installing Wget to
 cd
 echo Press any key to start installing
@@ -22,13 +23,13 @@ mkdir BloxstrapTemp
 cd BloxstrapTemp
 echo Press Any Key To Exit
 pause>nul
-rm nul
+del nul
 cd C:\
 rmdir BloxstrapTemp
 echo making Space In Free more a littile bit
 cd C:\
 cd Bloxstrap
-rm windowsdesktop-runtime-6.0.20-win-x64.exe
-rm Bloxstrap-v2.5.2.exe
+del windowsdesktop-runtime-6.0.20-win-x64.exe
+del Bloxstrap-v2.5.2.exe
 cd C:\
 rmdir Bloxstrap
