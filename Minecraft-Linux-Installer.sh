cba937b329858cfd637bedc93752ba1c998253e2
TouseefX <131637263+TouseefX@users.noreply.github.com>
2023-10-08 10:45:10 -0400

    Update Minecraft-Linux-Installer.sh
    for debain-based

--- Minecraft-Linux-Installer.sh
+++ Minecraft-Linux-Installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
  
-echo "Which Minecraft DO You Need?"
+echo "Which Minecraft DO You Need? Must Paste Sudo su to run this script correctly"
  
 select os in SKLaucherForLow TLaucher
 do
@@ -19,10 +19,17 @@ case $os in
      sudo apt install wget
      sudo apt update
      wget https://tlauncher.org/jar
-     rename /opt/TLDesktop/jar Minecraft.jar
+     cd $HOME
+     mkdir TXRunner
+     cd /opt/
+     cd TLDesktop
+     sudo mv jar ~/TXRunner
+     cd ~/TXRunner
+     sudo mv jar TL.jar
      echo "Creating Desktop For TL"
      sleep 5
      wget https://raw.githubusercontent.com/TouseefX/TouseefX/main/Run-App.sh
+     sudo mv Run-App.sh ~/TXRunner
      wget https://tlauncher.org/fav-pe-icon-512.png
      wget https://raw.githubusercontent.com/TouseefX/TouseefX/main/TL.desktop
      sudo mv /opt/TLDesktop/TL.desktop /usr/share/applications/
